d4a7d4db28c8b5ff34419cc871518aa9e263e933
Chris W. Gerber <chriswgerber@gmail.com>
2020-05-31 00:53:01 -0500

    Updates

--- curl/config.zsh
+++ curl/config.zsh
@@ -1,3 +1,5 @@
 # Curl
 
 # export CURL_CA_BUNDLE=$CA_BUNDLE
+
+-dot-add-path "${HOMEBREW_REPOSITORY}/opt/curl/bin"
--- gnupg/init.zsh
+++ gnupg/init.zsh
@@ -1,4 +1,4 @@
 -dot-add-symlink-to-home gnupg/gpg-agent.conf .gnupg/gpg-agent.conf
 -dot-add-symlink-to-home gnupg/gpg.conf .gnupg/gpg.conf
 
-init_gpg
+( init_gpg ) 1>/dev/null
