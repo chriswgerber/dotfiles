df30ba19668ef3ae35dd7a66183ebdbfec2e1383
Chris W. Gerber <chriswgerber@gmail.com>
2020-11-25 12:52:39 -0600

    Update post-config.zsh

--- post-config.zsh
+++ post-config.zsh
@@ -1,9 +1,6 @@
 #!/bin/zsh
 
-source "${ZSH}/oh-my-zsh.sh" # Init OMZ
-
-source "${DOTFILES_DIR}/theme.zsh"
-
+-dot-source-dotfile "${ZSH}/oh-my-zsh.sh" # Init OMZ
 
 if test -n "${ZSH_DEBUG}"; then # Load ZSH profiling mod
     zmodload zsh/zprof
